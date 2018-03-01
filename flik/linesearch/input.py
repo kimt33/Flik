6943ca3d707b96934fc4b8006e2f263808ba463f
Taewon D. Kim <kimt33@mcmaster.ca>
2018-03-01 12:38:51 -0500

    Use only keyword arguments when checking input
    What:
     1. Use keyword-only arugments in check_input
     2. Add keywords for func_val and grad_val
     3. Rewrite tests to check only minimal number of arguments at a time
    Why:
     1. Since not all of the arguments are used when check_input is called, they
        are turned into keyword arguments. Only keyword arguments are used for
        readability (and unambiguous API).
     2. func_val and grad_val need to be tested when func(val) and grad(val)
        are obtained.
     3. Since not all keywords are required, they can be tested separately.
        Testing separately is nicer b/c readability

--- flik/linesearch/input.py
+++ flik/linesearch/input.py
@@ -1,51 +1,95 @@
-""" Input processing of Line Search algorithm."""
-
+"""Input processing of line search module."""
+import itertools as it
 import numpy as np
 
-def check_input(var, func, grad, direction, alpha):
-    r"""Input control in Line Search algorithm
 
-    Parameters:
-    -----------
-    var: np.array((N,), dtype=float)
-        Variables' current values
-    func: callable
-        Function :math:`f(x)`
-    grad: callable
-        Gradient of the function :math:`\nabla f(x)`
-    direction: np.ndarray((N,), dtype=float) for N variables
-        Direction vector for next step
-    alpha: float
-        Step length
+def check_input(*, var=None, direction=None, alpha=None,
+                func=None, grad=None, func_val=None, grad_val=None):
+    r"""Test input used in line search module.
 
-    Raises:
-    -------
-    TypeError:
-        If var is not a float or a 1-D np.ndarray
-        If func is not a callable
-        If grad is not a callable
-        If direction is not a float or a 1-D np.ndarray
-        If alpha is not a float
-    ValueError:
-        If var and direction don't have the same shape
-        If alpha is outside the interval (0., 1.]
+    Parameters
+    ----------
+    var: {int, float, np.array((N,)), None}
+        Variables' current values.
+    func: {callable, None}
+        Function :math:`f(x)`.
+    grad: {callable, None}
+        Gradient of the function :math:`\nabla f(x)`.
+    func_val: {float, None}
+        Value of the function evaluated at var.
+    grad_val: {int, float, np.array((N,)), None}
+        Gradient evaluated at var.
+    direction: {int, float, np.ndarray((N,))}
+        Direction vector for next step.
+    alpha: {float, None}
+        Step length.
+
+    Raises
+    ------
+    TypeError
+        If var is not an int, float, or one dimensional numpy array.
+        If func is not a callable.
+        If grad is not a callable.
+        If func_val is not an int or float.
+        If grad_val is not an int, float, or one dimensional numpy array.
+        If direction is not an int, float, or one dimensional numpy array.
+        If alpha is not an int or float.
+    ValueError
+        If alpha is not in the interval (0, 1].
+        If var and direction do not have the same shape.
+        If var and grad_val do not have the same shape.
+        If direction and grad_val do not have the same shape.
+.
     """
-    if isinstance(var, (int,float)):
-        var = np.array(var, dtype=float, ndim=1)
+    if var is None:
+        pass
+    elif isinstance(var, (int, float)):
+        var = np.array(var, dtype=float, ndmin=1)
     elif not (isinstance(var, np.ndarray) and var.ndim == 1):
         raise TypeError("Variable vector should be a float or a 1-D numpy.ndarray")
-    if not callable(func):
+
+    if func is None:
+        pass
+    elif not callable(func):
         raise TypeError("func must be callable")
-    if not callable(grad):
+
+    if grad is None:
+        pass
+    elif not callable(grad):
         raise TypeError("grad must be callable")
-    if isinstance(direction, (int, float)):
-        direction = np.array(direction, dtype=float, ndim=1)
+
+    if func_val is None:
+        pass
+    elif not isinstance(func_val, (int, float)):
+        raise TypeError('func_val must be a float.')
+
+    if grad_val is None:
+        pass
+    elif isinstance(grad_val, (int, float)):
+        grad_val = np.array(grad_val, dtype=float, ndmin=1)
+    elif not (isinstance(grad_val, np.ndarray) and grad_val.ndim == 1):
+        raise TypeError('grad_val must be a one dimensional numpy array.')
+
+    if direction is None:
+        pass
+    elif isinstance(direction, (int, float)):
+        direction = np.array(direction, dtype=float, ndmin=1)
     elif not (isinstance(direction, np.ndarray) and direction.ndim == 1):
-        raise TypeError("The direction vector should be provided as a float or"
-                        " a numpy.ndarray")
-    if not isinstance(alpha, float):
+        raise TypeError("The direction vector should be provided as a float or a numpy array")
+
+    if alpha is None:
+        pass
+    elif not isinstance(alpha, (int, float)):
         raise TypeError("Alpha should be provided as a float")
-    if var.shape != direction.shape:
-        raise ValueError("The shape of vec and direction should be the same")
-    if not  0. < alpha <= 1.0:
-        raise ValueError("Alpha's value should be in the interval (0., 1.]")
+    elif not 0 < alpha <= 1:
+        raise ValueError("Alpha value should be in the interval (0., 1.]")
+
+    name_dict = {'var': var, 'direction': direction, 'grad_val': grad_val}
+    for name1, name2 in it.combinations(name_dict, 2):
+        array1 = name_dict[name1]
+        array2 = name_dict[name2]
+
+        if array1 is None or array2 is None:
+            continue
+        if array1.shape != array2.shape:
+            raise ValueError(f'{name1} and {name2} must have the same shape.')
--- flik/linesearch/test/test_input.py
+++ flik/linesearch/test/test_input.py
@@ -1,44 +1,80 @@
-"""Test for flink.linesearch.input."""
-
+"""Test for flik.linesearch.input."""
 import numpy as np
 from nose.tools import assert_raises
-
 from flik.linesearch.input import check_input
 
+
 def test_input_control():
-    """Tests for Line Search parameters"""
-    def func0(var):
+    """Test linesearch.input.check_input."""
+    def func(var):
         r"""Function :math:`\sum_i x_i^2`"""
         return np.sum(var**2)
 
-    def grad0(var):
+    def grad(var):
         r"""Gradient of sum_square :math:`\nabla f(x)`"""
         return 2*var
 
-    # Check var vector
-    direction = np.array([2., 1., 3.])
-    alpha = 0.1
-    var = [1]
-    assert_raises(TypeError, check_input, var, func0, grad0, direction, alpha)
-    var = np.array([[1., 3.],[2., 1.]])
-    assert_raises(TypeError, check_input, var, func0, grad0, direction, alpha)
-    # Check for function and gradient
-    var = np.array([2., 1., 3.])
-    func1 = [0.1]
-    assert_raises(TypeError, check_input, var, func1, grad0, direction, alpha)
-    grad1 = 2.
-    assert_raises(TypeError, check_input, var, func0, grad1, direction, alpha)
-    # Check direction
-    direction = [1, 2, 3]
-    assert_raises(TypeError, check_input, var, func0, grad0, direction, alpha)
-    direction = np.array([[1, 2],[5, 3]])
-    assert_raises(TypeError, check_input, var, func0, grad0, direction, alpha)
-    # Check alpha
-    direction = np.array([2., 1., 3.])
-    alpha = 2
-    assert_raises(TypeError, check_input, var, func0, grad0, direction, alpha)
-    alpha = 1.2
-    assert_raises(ValueError, check_input, var, func0, grad0, direction, alpha)
-    alpha = -1.2
-    assert_raises(ValueError, check_input, var, func0, grad0, direction, alpha)
-    assert check_input(var, func0, grad0, direction, 0.2) is None
+    # check var
+    assert_raises(TypeError, check_input, var=[1])
+    assert_raises(TypeError, check_input, var=np.array([[1, 2]]))
+    check_input(var=0)
+    check_input(var=0.0)
+    check_input(var=np.array([0]))
+
+    # check func
+    assert_raises(TypeError, check_input, func='1')
+    check_input(func=lambda x: x)
+
+    # check grad
+    assert_raises(TypeError, check_input, grad='1')
+    check_input(func=lambda x: x)
+
+    # check func_val
+    assert_raises(TypeError, check_input, func_val=np.array([1]))
+    check_input(func_val=1)
+    check_input(func_val=1.0)
+
+    # check grad_val
+    assert_raises(TypeError, check_input, grad_val=[1])
+    assert_raises(TypeError, check_input, direction=np.array(1))
+    check_input(grad_val=1)
+    check_input(grad_val=1.0)
+    check_input(grad_val=np.array([1]))
+
+    # check direction
+    assert_raises(TypeError, check_input, direction=[1])
+    assert_raises(TypeError, check_input, direction=np.array(1))
+    check_input(direction=1)
+    check_input(direction=1.0)
+    check_input(direction=np.array([1]))
+
+    # check alpha
+    assert_raises(TypeError, check_input, alpha=np.array([1]))
+    assert_raises(ValueError, check_input, alpha=2)
+    assert_raises(ValueError, check_input, alpha=0)
+    assert_raises(ValueError, check_input, alpha=-1)
+    check_input(alpha=1)
+    check_input(alpha=1.0)
+    check_input(alpha=0.5)
+
+    # check var and direction
+    assert_raises(ValueError, check_input, var=1, direction=np.array([2, 3]))
+    assert_raises(ValueError, check_input, var=np.array([1]), direction=np.array([2, 3]))
+    check_input(var=1, direction=np.array([2]))
+
+    # check val and grad_val
+    assert_raises(ValueError, check_input, var=np.array([2, 3]), grad_val=1)
+    assert_raises(ValueError, check_input, var=np.array([2, 3]), grad_val=np.array([1]))
+    check_input(var=np.array([2]), grad_val=1)
+
+    # check grad_val and direction
+    assert_raises(ValueError, check_input, grad_val=1, direction=np.array([2, 3]))
+    assert_raises(ValueError, check_input, grad_val=np.array([1]), direction=np.array([2, 3]))
+    check_input(grad_val=1, direction=np.array([2]))
+
+    # check var, grad_val and direction
+    assert_raises(ValueError, check_input, var=1, grad_val=1, direction=np.array([2, 3]))
+    check_input(var=0, grad_val=1, direction=np.array([2]))
+
+    # do nothing
+    check_input()
